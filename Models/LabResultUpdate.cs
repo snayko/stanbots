3c80808518b344104a1deb79b546966c332247ea
Naiko, Stanislav (Heidelberg) DEU <stanislav.naiko@heidelbergmaterials.com>
2023-10-06 08:12:54 +0200

    add missing file

---
+++ Models/LabResultUpdate.cs
@@ -0,0 +1,10 @@
+namespace stanbots.Models;
+
+public class LabResultUpdate
+{
+    public string LabName { get; set; }
+    public string StudentName { get; set; }
+    public string StudentEmail { get; set; }
+    public string GroupName { get; set; }
+    public string Result { get; set; }
+}
